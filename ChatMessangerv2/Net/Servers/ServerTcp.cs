c8cc12baf2315ac7dab0f835a1c9b739e6446e3c
BO0zer <yurgroshev@edu.hse.ru>
2022-06-14 23:14:12 +0500

    п

--- ChatMessangerv2/Net/Servers/ServerTcp.cs
+++ ChatMessangerv2/Net/Servers/ServerTcp.cs
@@ -24,7 +24,7 @@ namespace ChatMessangerv2.Net
         public ServerTcp()
         {
             _tcpClient = new TcpClient();
-            GetMessages();
+            //GetMessages();
         }
         public void ConnectToServer(string username)
         {
